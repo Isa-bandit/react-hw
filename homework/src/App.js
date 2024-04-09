f16a0f2cf3f7567dd58b3a7d17db8d7d92120696
Isa-bandit <128226535+Isa-bandit@users.noreply.github.com>
2024-04-10 00:50:15 +0600

    Add files via upload

---
+++ homework/src/App.js
@@ -0,0 +1,41 @@
+import logo from './logo.svg';
+import './App.css';
+
+function Title() {
+  return(
+    <div>
+    <h1>Title</h1>
+  </div>
+  )
+
+}
+function Header() {
+  return(
+   <div><Title/></div>
+  )
+}
+function Footer() {
+   return(
+   <div><Title/></div>
+   )
+}
+function Content() {
+  return(
+   <div><Title/></div> 
+  )
+}
+
+
+function App() {
+  return (
+    <div>
+      <Header/>
+      <Content/>
+      <Footer/>
+      
+    </div>
+  );
+}
+
+
+export default App;
