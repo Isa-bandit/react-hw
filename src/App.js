24fbe8bad227e1b28f33f3e3ff96cb43ee67b70f
Isa-bandit <isaabdraev5@gmail.com>
2024-04-24 16:28:12 +0600

    modified:   src/App.js

--- src/App.js
+++ src/App.js
@@ -3,8 +3,6 @@ import { AboutPage } from './pages/aboutPage/AboutPage';
 import { MainPage } from './pages/mainPage/MainPage';
 import { TodosPage } from './pages/todosPage/TodosPage';
 
-
-
 function App() {
   return (
     <div>
